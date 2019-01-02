3beb7508fa00a8054f5bef3c6e502b7dbea11db8
Craig Ambrose <craig@craigambrose.com>
2019-01-02 14:38:31 +1100

    test cleanup

---
+++ spec/rostering/roster_builder_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'rostering/roster_builder'
+require 'rostering/nurse'
+
+module Rostering
+  describe RosterBuilder do
+    include DateSpecHelpers
+
+    describe '#build' do
+      it 'You might like to write some tests for the RosterBuilder build method'
+    end
+  end
+end
