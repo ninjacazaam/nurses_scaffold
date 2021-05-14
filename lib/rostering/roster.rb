edfe6dee296083141dcfb9b5f4a4f8c2abfc143b
Andy Nicholson <anicholson@squareup.com>
2021-05-14 11:51:36 +1000

    Fix typo

--- lib/rostering/roster.rb
+++ lib/rostering/roster.rb
@@ -1,5 +1,5 @@
 module Rostering
-  # Some sort of data stucture representing the roster
+  # Some sort of data structure representing the roster
   class Roster
     def each_shift
       raise NotImplementedError, 'You need to implement this method so the output formatter can work'
