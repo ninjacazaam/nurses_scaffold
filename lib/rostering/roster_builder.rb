9666a47688c745500fd047a208b7b88ca0db4941
Corey Ellis <coreypozzatiellis@gmail.com>
2022-10-10 18:17:24 +1100

    Refactored Roster builder code

--- lib/rostering/roster_builder.rb
+++ lib/rostering/roster_builder.rb
@@ -15,19 +15,17 @@ module Rostering
 
     def build(date_range, all_nurses)
       daily_required_nurse_count = @config[:shift_names].count * @config[:nurses_per_shift]
-      roster_dates = {}
       raise "Not enough Nurses for your shifts" if all_nurses.count < daily_required_nurse_count
+      roster_dates = {}
       date_range.each do |date|
-        nurses_for_today = all_nurses
+        nurses_for_today = all_nurses.map(&:clone)
         shifts = {}
         @config[:shift_names].each do |shift|
           shifts[shift.to_sym] = nurses_for_today.shift(@config[:nurses_per_shift])
-          nurses_for_today.concat(shifts[shift.to_sym])
         end
           roster_dates[date] = shifts
       end
       Roster.new(roster_dates)
-      # binding.pry
     end
   end
 end
